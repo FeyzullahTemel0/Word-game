e3e7a2a3e579ade95cb79d5e9b9b821be8ffb18a
Feyzullah Temel <109575132+FeyzullahTemel0@users.noreply.github.com>
2024-01-07 17:19:55 +0000

    Update Kelime Oyunu.py

--- Kelime Oyunu.py
+++ Kelime Oyunu.py
@@ -1,52 +1,52 @@
-
-# Kelime Tahmin Oyunu
 import random
 
+def load_word_list():
+    with open("word_list.txt", "r") as file:
+        word_list = file.read().splitlines()
+    return word_list
 
 def pick_random_word():
-    word_list = ["python", "c", "java", "swift", "html", "css", "go", "ruby"]
+    word_list = load_word_list()
     random_word = random.choice(word_list)
-    return random_word
-
+    return random_word.upper()
 
 def make_word_classified(word):
-    classified_list = ["_" for i in word]
+    classified_list = ["_" if letter.isalpha() else letter for letter in word]
     return classified_list
-
 
 def guess():
     word = pick_random_word()
     classified_word = make_word_classified(word)
-    print(*classified_word)
+    print(" ".join(classified_word))
     total_attempts = 0
+    max_attempts = int(input("Enter the number of attempts you want to have: "))
 
-    while True:
+    while total_attempts < max_attempts:
         try:
-            answer = input("Guess a letter (Write only one letter)>: ").upper()
-            if len(answer) > 1:
-                raise Exception
-        except Exception:
-            print("Only one letter at a time!")
+            answer = input("Guess a letter (Write only one letter): ").upper()
+            if len(answer) != 1 or not answer.isalpha():
+                raise ValueError
+        except ValueError:
+            print("Invalid input. Please enter a single letter.")
             continue
+
         total_attempts += 1
 
-        if total_attempts >= 7:
-            print("Sorry but you lost!")
-            try_again = input("Wanna play again? (write y or n) >: ")
-            if try_again == 'y':
-                guess()
-            elif try_again == 'n':
-                print("Goodbye!")
-                quit()
-
-        for i in range(len(word)):
+        if answer in word:
+            for i in range(len(word)):
                 if answer == word[i]:
                     classified_word[i] = answer
-                if "".join(classified_word) == word:
-                    print("You won!")
-                    quit()
+            print(" ".join(classified_word))
+            if "".join(classified_word) == word:
+                print("Congratulations! You won!")
+                break
+        else:
+            print("Incorrect guess. Try again.")
+        
+        print(f"Total attempts left: {max_attempts - total_attempts}")
 
-        print(*classified_word, f"\nTotal attempts left: {7 - total_attempts}")
+    else:
+        print(f"Sorry, you've run out of attempts. The word was '{word}'.")
 
-
-guess()
+if __name__ == "__main__":
+    guess()
